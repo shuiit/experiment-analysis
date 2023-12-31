966ff29788db116641e62533da5348b746648aa0
roni-maya <roni.maya@mail.huji.ac.il>
2023-12-31 18:05:31 +0200

    CLIP FRAME

--- pre_process_csv/PreProcess.py
+++ pre_process_csv/PreProcess.py
@@ -16,10 +16,16 @@ class PreProcess:
 
     def manual_clip_frames(self):
         fig, ax = plt.subplots()
+    
+        phi,  = ax.plot(self.angles['frames_frame'],self.angles['phi_rw'])
+        ax_twin = ax.twinx()
 
-        ax.plot(self.angles['frames_frame'],self.angles['phi_rw'])
-        ax.plot(self.angles['frames_frame'],self.angles['pitch_body'])
+        pitch,  = ax_twin.plot(self.angles['frames_frame'],self.angles['pitch_body'],color = 'red')
+        zero_frame = self.angles['frames_frame'][self.angles['timeframe_time']==0]
+        if len(zero_frame)>0: ax_twin.axvline(x = zero_frame.iloc[0], color = 'red',linewidth = 5)
+        ax_twin.autoscale()
+        ax_twin.relim()
+       
         x = plt.ginput(2)
         self.angles = self.angles[(self.angles['frames_frame']>=int(x[0][0])) & (self.angles['frames_frame']<=int(x[1][0]))]
         self.vectors = self.vectors[(self.vectors['frames_frame']>=int(x[0][0])) & (self.vectors['frames_frame']<=int(x[1][0]))]
-        
